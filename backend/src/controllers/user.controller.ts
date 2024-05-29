c1cc4f2402d728d18fb95d774a457746abca9532
avinashChandavar7 <avinashChnadavar7@gmail.com>
2024-05-29 19:51:14 +0530

    feat : add getUserById controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,1 +1,45 @@
-export { };
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+import { asyncHandler } from "../utils/asyncHandler";
+
+import User from "../models/users.model";
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  //#swagger.tags = ['User']
+
+  const userId = req.userId;
+
+  const user = await User.findById(userId).select("-password");
+
+  if (!user) {
+    throw new ApiError(400, "Invalid user")
+  }
+
+  return res.status(200)
+    .json(new ApiResponse(200, { user }, "get User Details successfully "));
+});
+
+
+
+const getUserById = asyncHandler(async (req, res) => {
+  //#swagger.tags = ['User']
+
+  const id = req.params.id.toString();
+
+  const user = await User.findById(id);
+
+  if (!user) {
+    throw new ApiError(400, "User not Found");
+  }
+
+  return res.status(200)
+    .json(new ApiResponse(200, { user }, "get User Details successfully "));
+});
+
+
+
+export {
+  getCurrentUser,
+
+  getUserById,
+};
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,11 +1,22 @@
 import { Router } from "express";
 
+import verifyToken from "../middleware/auth.middleware";
 
-import { } from "../controllers/user.controller";
+import {
+  getCurrentUser,
 
+  getUserById,
+} from "../controllers/user.controller";
 
 
 const router = Router();
+
+router.get("/current-user", verifyToken, getCurrentUser);
+
+router
+  .route('/:id')
+  .get(getUserById)
+
 
 
 
