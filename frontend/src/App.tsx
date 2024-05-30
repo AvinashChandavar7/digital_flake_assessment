34a204e9d876bbfa5d63c547c6349ffc58246aa2
avinashChandavar7 <avinashChnadavar7@gmail.com>
2024-05-30 13:43:24 +0530

    feat : add RolesPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { Navigate, Route, Routes, } from "react-router-dom"
 import Layout from "./layouts/Layout"
 import HomePage from "./pages/HomePage"
+import UsersPage from "./pages/UsersPage"
+import RolesPage from "./pages/RolesPage"
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout><HomePage /></Layout>} />
-
+      <Route path="/users" element={<Layout><UsersPage /></Layout>} />
+      <Route path="/roles" element={<Layout><RolesPage /></Layout>} />
 
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
